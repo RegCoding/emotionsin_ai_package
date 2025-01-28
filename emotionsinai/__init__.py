0fe555de8bd1ea12a8da86666d12730da880b04a
RegCode <gerritknippschild@gmail.com>
2025-01-28 11:07:35 +0100

    updated init.py

--- emotionsinai/__init__.py
+++ emotionsinai/__init__.py
@@ -2,5 +2,7 @@ from .base_llm import BaseLLM
 from .conversation_repository import ConversationRepository
 from .emotion_services import EmotionServices
 from .openai_provider import OpenAIProvider
+from .inner_state import InnerState
+from .user_emotional_profile import UserEmotionalProfile
 
-__all__ = ["BaseLLM", "ConversationRepository" "EmotionServices", "OpenAIProvider"]
+__all__ = ["BaseLLM", "ConversationRepository" "EmotionServices", "OpenAIProvider", "InnerState", "UserEmotionalProfile"]
